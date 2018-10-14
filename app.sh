c61a1ffb358693fb7089289771e2ffc335737785
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-10-14 14:39:11 +0200

    Install virtualenv under user

--- app.sh
+++ app.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 curl https://bootstrap.pypa.io/get-pip.py | python2 - --user
-python2 -m pip install virtualenv
+python2 -m pip install virtualenv --user
 python2 -m virtualenv py2venv
 py2venv/bin/pip install ansible-review
 
