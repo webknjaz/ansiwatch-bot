5f26ba2b3acbb2ef8d2d2f40f2b9b467c0254e8d
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-10-14 22:52:20 +0200

    Drop a default handler out of root

--- ansiwatch_bot/apps/root.py
+++ ansiwatch_bot/apps/root.py
@@ -5,7 +5,3 @@ class RootBotApp:
     @cherrypy.expose
     def index(self, *args, **kwargs):
         return 'Hi from ansiwatch bot index!'
-
-    @cherrypy.expose
-    def default(self, *args, **kwargs):
-        return 'Hi from ansiwatch bot default handler!'
