6fcda983e29bc9a5d0c3dda18d1251067db44262
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-10-14 23:53:51 +0200

    Use shimmed way of running git in a separate dir
    Ref: https://stackoverflow.com/a/20115526/595220
    Ref: git/git@44e1e4d
    Ref: git/git@6a536e2

--- ansiwatch_bot/workers.py
+++ ansiwatch_bot/workers.py
@@ -6,7 +6,16 @@ import cherrypy
 GITHUB_REPO_URL_TMPL = 'git://github.com/{slug}.git'
 
 
+def get_git_separate_dir_args(path):
+    path_str = str(path)
+    return f'--git-dir={path_str}/.git', f'--work-tree={path_str}/'
+    return '-C', path_str  # This'd work in Git v1.8.5+
+    # Post note: since Git v2.3.4+ it processes empty path arg correctly
+
+
 def sync_repo(repo, path):
+    git_args = get_git_separate_dir_args(path)
+
     cherrypy.engine.log(f'Starting to sync repo {repo}...')
     if not (path / '.git').exists():
         cherrypy.engine.log(f'Cloning repo {repo}...')
@@ -15,18 +24,18 @@ def sync_repo(repo, path):
         )
         cherrypy.engine.log(f'Adding refs/pull/*/head to repo {repo} config...')
         subprocess.check_output(
-            ('git', '-C', str(path), 'config', '--add', 'remote.origin.fetch',
+            ('git', *git_args, 'config', '--add', 'remote.origin.fetch',
              '+refs/pull/*/head:refs/pull/origin/*')
         )
         cherrypy.engine.log(f'Adding refs/pull/*/merge to repo {repo} config...')
         subprocess.check_output(
-            ('git', '-C', str(path), 'config', '--add', 'remote.origin.fetch',
+            ('git', *git_args, 'config', '--add', 'remote.origin.fetch',
              '+refs/pull/*/merge:refs/pull/origin/*')
         )
 
     cherrypy.engine.log(f'Fetching repo {repo}...')
     subprocess.check_output(
-        ('git', '-C', str(path), 'fetch', '--all')
+        ('git', *git_args, 'fetch', '--all')
     )
 
 
