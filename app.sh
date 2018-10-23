647b2fe6b8d07003808271e5a12ee26b57199033
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-10-23 15:48:34 +0200

    Make ansible-review env set up asynchronously

--- app.sh
+++ app.sh
@@ -2,10 +2,13 @@
 
 git --version
 
-curl https://bootstrap.pypa.io/get-pip.py | python2 - --user
-python2 -m pip install virtualenv --user
-python2 -m virtualenv py2venv
-py2venv/bin/pip install ansible-review
+{
+    >&2 echo Starting to set up python2 env for ansible-review asynchronously...
+    curl https://bootstrap.pypa.io/get-pip.py | python2 - --user
+    python2 -m pip install virtualenv --user
+    python2 -m virtualenv py2venv
+    py2venv/bin/pip install ansible-review
+} &
 
 echo "Port: $PORT"
 exec python -m "$APP_MODULE"
